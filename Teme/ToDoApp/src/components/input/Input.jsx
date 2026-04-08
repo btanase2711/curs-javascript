9bab65f54c6e5271461f0eae0c1007a69ca511e9
btanase2711 <132082667+btanase2711@users.noreply.github.com>
2026-04-08 10:36:40 +0300

    Delete Teme directory

--- Teme/ToDoApp/src/components/input/Input.jsx
+++
@@ -1,8 +0,0 @@
-import "./Input.css";
-
-const Input = (props) => {
-  return <input className="primary-input" {...props} />;
-};
-
-
-export default Input;
--- Teme/ToDoApp/src/components/input/TextArea.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Input.css";
-
-const TextArea = (props) => {
-  return <textarea className="primary-input textarea" rows="3" {...props} />;
-};
-
-export default TextArea;
